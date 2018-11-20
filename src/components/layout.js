8df9a2f624c59a89cea87febcb212729ac92f1dd
Koki Takahashi <hakatasiloving@gmail.com>
2018-11-20 17:34:05 +0900

    Fix metas

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,9 +4,12 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import '../assets/scss/main.scss'
+import ogImage from '../images/og_image.png'
 
 const Layout = ({ children, location }) => {
   const content = <div>{children}</div>
+  const description =
+    '東京大学TSGがお送りする3日間限りのライブプログラミングのイベント TSG LIVE! 2。放送はYouTube/ニコニコ生放送で同時中継されます！'
 
   return (
     <StaticQuery
@@ -24,8 +27,27 @@ const Layout = ({ children, location }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
+              { name: 'description', content: description },
+              {
+                name: 'keywords',
+                content:
+                  '東京大学,プログラミング,生放送,ライブプログラミング,ゲームプログラミング,AIプログラミング,CTF',
+              },
+              { property: 'fb:app_id', content: '1864540247116178' },
+              { property: 'og:title', content: data.site.siteMetadata.title },
+              { property: 'og:type', content: 'website' },
+              { property: 'og:url', content: 'http://tsg.ne.jp/live' },
+              { property: 'og:image', content: ogImage },
+              { property: 'og:image:width', content: '1200' },
+              { property: 'og:image:height', content: '630' },
+              { property: 'og:description', content: description },
+              { property: 'og:site_name', content: 'TSG LIVE! 2' },
+              { name: 'twitter:card', content: 'summary' },
+              { name: 'twitter:site', content: '@tsg_ut' },
+              { name: 'twitter:creator', content: '@tsg_ut' },
+              { name: 'twitter:title', content: data.site.siteMetadata.title },
+              { name: 'twitter:description', content: description },
+              { name: 'twitter:image', content: ogImage },
             ]}
           >
             <html lang="en" />
