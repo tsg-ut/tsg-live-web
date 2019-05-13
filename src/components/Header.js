9991d1d0a0ec2bec59e679436e613c5d0aef978c
Koki Takahashi <hakatasiloving@gmail.com>
2019-05-13 17:09:28 +0900

    Update broadcast links

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,29 +12,37 @@ const Header = props => {
             src={logo}
             alt="東大生が送るラブプログラミングショー TSG LIVE! 3"
           />
-          <p>5月18日 (土), 19日 (日)</p>
+        <p>5月18日 (土), 19日 (日) 10:00 ～ 17:30</p>
           <p>
             東京大学五月祭 工学部3号館4階 <wbr/>440（35号講義室）にてオンステージ
             <br />
             YouTube Live/ニコニコ生放送にて<wbr/>インターネット同時中継
           </p>
-          {/* <p style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>
-            放送終了しました。<wbr/>ご視聴ありがとうございました!!
-          </p>
           <div className="watch-links">
             <a
               className="watch-link youtube"
-              href="https://www.youtube.com/channel/UCzCqJL3-RdhTw9WHYDbPO4Q/videos"
+              href="https://www.youtube.com/watch?v=XUDcmuFmWi4"
               target="_blank"
               rel="noopener"
             >
               <div className="watch-link-inner youtube">
-                YouTubeで
-                <br />
-                録画視聴する
+                YouTube Live で<br />
+                視聴予約する (1日目)
               </div>
             </a>
-          </div> */}
+            <a
+              className="watch-link niconico"
+              href="https://live.nicovideo.jp/watch/lv320049023"
+              target="_blank"
+              rel="noopener"
+            >
+              <div className="watch-link-inner niconico">
+                ニコニコ生放送で
+                <br />
+                視聴予約する (1日目)
+              </div>
+            </a>
+          </div>
         </div>
       </div>
       <nav>
