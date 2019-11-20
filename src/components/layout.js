4d6f27dac63b656b8c1a40f7a9bb23d0e3cf017b
hideo54 <hideo54@hideo54.com>
2019-11-20 17:04:19 +0900

    description は data.siteMetadata.description で一元化

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,58 +7,52 @@ import '../assets/scss/main.scss'
 import twitterCardImage from '../images/twitter_card_image.png'
 import ogImage from '../images/og_image.png'
 
-const Layout = ({ children, location }) => {
-  const content = <div>{children}</div>
-  const description =
-    '東京大学TSGがお送りする3日間限りのライブプログラミングのイベント TSG LIVE! 4。放送はYouTube/ニコニコ生放送で同時中継されます！'
-
-  return (
-    <StaticQuery
-      query={graphql`
-        query SiteTitleQuery {
-          site {
-            siteMetadata {
-              title
-            }
+const Layout = ({ children, location }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
           }
         }
-      `}
-      render={data => (
-        <>
-          <Helmet
-            title={data.site.siteMetadata.title}
-            meta={[
-              { name: 'description', content: description },
-              {
-                name: 'keywords',
-                content:
-                  '東京大学,プログラミング,生放送,ライブプログラミング,ゲームプログラミング,AIプログラミング,CTF',
-              },
-              { property: 'fb:app_id', content: '1864540247116178' },
-              { property: 'og:title', content: data.site.siteMetadata.title },
-              { property: 'og:type', content: 'website' },
-              { property: 'og:url', content: 'https://live.tsg.ne.jp/' },
-              { property: 'og:image', content: new URL(ogImage, 'https://live.tsg.ne.jp/') },
-              { property: 'og:image:width', content: '1200' },
-              { property: 'og:image:height', content: '630' },
-              { property: 'og:description', content: description },
-              { property: 'og:site_name', content: 'TSG LIVE! 4' },
-              { name: 'twitter:card', content: 'summary_large_image' },
-              { name: 'twitter:site', content: '@tsg_ut' },
-              { name: 'twitter:creator', content: '@tsg_ut' },
-              { name: 'twitter:title', content: data.site.siteMetadata.title },
-              { name: 'twitter:description', content: description },
-              { name: 'twitter:image', content: new URL(twitterCardImage, 'https://live.tsg.ne.jp/') },
-            ]}
-          >
-            <html lang="ja" />
-          </Helmet>
-          {content}
-        </>
-      )}
-    />
-  )
-}
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: data.siteMetadata.description },
+            {
+              name: 'keywords',
+              content:
+                '東京大学,プログラミング,生放送,ライブプログラミング,ゲームプログラミング,AIプログラミング,CTF',
+            },
+            { property: 'fb:app_id', content: '1864540247116178' },
+            { property: 'og:title', content: data.site.siteMetadata.title },
+            { property: 'og:type', content: 'website' },
+            { property: 'og:url', content: 'https://live.tsg.ne.jp/' },
+            { property: 'og:image', content: new URL(ogImage, 'https://live.tsg.ne.jp/') },
+            { property: 'og:image:width', content: '1200' },
+            { property: 'og:image:height', content: '630' },
+            { property: 'og:description', content: data.siteMetadata.description },
+            { property: 'og:site_name', content: 'TSG LIVE! 4' },
+            { name: 'twitter:card', content: 'summary_large_image' },
+            { name: 'twitter:site', content: '@tsg_ut' },
+            { name: 'twitter:creator', content: '@tsg_ut' },
+            { name: 'twitter:title', content: data.site.siteMetadata.title },
+            { name: 'twitter:description', content: data.siteMetadata.description },
+            { name: 'twitter:image', content: new URL(twitterCardImage, 'https://live.tsg.ne.jp/') },
+          ]}
+        >
+          <html lang="ja" />
+        </Helmet>
+        <div>{children}</div>
+      </>
+    )}
+  />
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
