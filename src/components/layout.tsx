cb434f39994d832476cf493b0882fbe614a7e0c5
hideo54 <hideo54@hideo54.com>
2019-11-20 17:06:23 +0900

    Use TypeScript for layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
@@ -53,9 +52,5 @@ const Layout = ({ children, location }) => (
     )}
   />
 );
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
 
 export default Layout
