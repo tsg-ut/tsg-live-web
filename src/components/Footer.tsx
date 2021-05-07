96abb9cf5d46749344183ef87bcf47942a7d34ed
hideo54 <hideo54@hideo54.com>
2021-05-07 22:12:14 +0900

    Change background image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ const A = (props: { href: string, children: string }) => (
 const Footer = (props: FooterProps) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <A href="https://html5up.net">HTML5 UP</A>. Built with: <A href="https://www.gatsbyjs.org/">Gatsby.js</A></p>
-       <p className="copyright"><A href="https://pixabay.com/ja/photos/%E6%97%A5%E6%B2%A1-%E3%83%96%E3%83%AA%E3%83%83%E3%82%B8-%E7%BE%8E%E3%81%97%E3%81%84-%E5%A4%95%E6%9A%AE%E3%82%8C-4872552/">Background image</A> is by <A href="https://pixabay.com/ja/users/koma_photo-15194464/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4872552">Kanta Komazawa</A>.</p>
+       <p className="copyright"><A href="https://www.flickr.com/photos/57435451@N06/21009430635">Background image</A> is by <A href="https://www.flickr.com/people/57435451@N06/">汪 大雄</A> (<A href='https://creativecommons.org/licenses/by-sa/2.0/'>CC BY-SA 2.0</A>).</p>
     </footer>
 )
 
